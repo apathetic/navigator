290a46e90bdc2ab087d760ee5c7828221c71f6c0
Wes Hatch <wes@apathetic-2.local>
2015-11-05 23:10:27 -0500

    updating demo

--- test/nav.spec.js
+++ test/nav.spec.js
@@ -1,23 +1,7 @@
-describe('parallax', function() {
+describe('navigator', function() {
 
-	it('should should export to the window object', function () {
+	it('should do something', function () {
 		// expect(...
-	});
-
-	it('should should export as a common module', function () {
-
-	});
-
-	it('should should export as an AMD module', function () {
-
-	});
-
-	it('initializes correctly', function () {
-
-	});
-
-	it('accepts the correct options', function () {
-		// range, mode, el
 	});
 
 });
